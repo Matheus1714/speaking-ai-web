207b0c62cc72c2d060b050efaf7e832baf812d1d
matheus1714 <matheussilvamartins1714@gmail.com>
2024-01-04 11:28:33 -0300

    create class to control record

---
+++ scripts/record.js
@@ -0,0 +1,65 @@
+const MicroIcons = {
+    PLAY: 'mic-outline',
+    STOP: 'square-outline'
+}
+
+export class Record {
+    constructor() {
+        this.audioPlayer = document.getElementById('audioPlayer')
+        this.recordAudioControl = document.getElementById('recordAudioControl')
+        this.media = document.getElementById('media')
+        
+        this.mediaRecorder = null
+        this.audioChunks = []
+        this.isRecording = false
+
+        this.recordAudioControl.addEventListener('click', () => this.toggleRecording())
+    }
+
+    async startRecording() {
+        navigator.mediaDevices.getUserMedia({ audio: true })
+            .then((stream) => {
+                this.mediaRecorder = new MediaRecorder(stream)
+
+                this.mediaRecorder.ondataavailable = (e) => {
+                    if (e.data.size > 0) {
+                        this.audioChunks.push(e.data)
+                    }
+                };
+
+                this.mediaRecorder.onstop = () => {
+                    const audioBlob = new Blob(this.audioChunks, { type: 'audio/wav' })
+                    this.audioChunks = []
+                    this.audioPlayer.src = URL.createObjectURL(audioBlob)
+                };
+
+                this.mediaRecorder.start()
+            })
+            .catch((error) => {
+                console.error('Erro ao obter permissão de áudio: ', error)
+            })
+    }
+
+    async stopRecording() {
+        if (this.mediaRecorder && this.mediaRecorder.state !== 'inactive') {
+            this.mediaRecorder.stop();
+            this.audioPlayer.addEventListener('loadedmetadata', () => {
+                this.audioPlayer.play();
+            });
+        }
+    }
+
+    async toggleRecording() {
+        const recordAudioControlIcon = this.recordAudioControl.querySelector('ion-icon')
+
+        if (!this.isRecording) {
+            recordAudioControlIcon.setAttribute('name', MicroIcons.STOP)
+            await this.startRecording()
+        } else {
+            recordAudioControlIcon.setAttribute('name', MicroIcons.PLAY)
+            await this.stopRecording()
+            this.audioPlayer.play()
+        }
+        this.isRecording = !this.isRecording
+    }
+}
